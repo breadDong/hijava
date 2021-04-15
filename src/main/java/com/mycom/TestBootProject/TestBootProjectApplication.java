d435f822c1457464f821039902aa1c3db9842d8c
JiDongGu <breadday@hanmail.net>
2021-04-15 17:43:46 +0900

    System.out.println

--- src/main/java/com/mycom/TestBootProject/TestBootProjectApplication.java
+++ src/main/java/com/mycom/TestBootProject/TestBootProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestBootProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TestBootProjectApplication!!!");
 		SpringApplication.run(TestBootProjectApplication.class, args);
 	}
-
 }
