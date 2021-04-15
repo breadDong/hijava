e3df8776a083cd8e5a3227546b665ee6053c6261
JiDongGu <breadday@hanmail.net>
2021-04-15 17:39:52 +0900

    초기 커밋

---
+++ src/main/java/com/mycom/TestBootProject/TestBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.TestBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycom/TestBootProject/TestBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.TestBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
